d8f4848c1f227ab5c7e59a26f2fe1ad7cd731ef0
dreamhone <dreamhone@gmail.com>
2021-03-11 08:20:17 -0800

    live 12/3/2021

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitdad1e30410739153b913ff51e0e2f1cb::getLoader();
+return ComposerAutoloaderInitecd889bb68d63334e737994c9ea9ddcd::getLoader();
